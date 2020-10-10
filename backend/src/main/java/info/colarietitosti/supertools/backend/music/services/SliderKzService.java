a45deb55816f58ba2790985b63d2f61ca70d6b8e
cola <Wlafica1!>
2020-10-10 23:01:31 +0200

    refactored SliderKzService

--- backend/src/main/java/info/colarietitosti/supertools/backend/music/services/SliderKzService.java
+++ backend/src/main/java/info/colarietitosti/supertools/backend/music/services/SliderKzService.java
@@ -13,30 +13,41 @@ import static java.lang.Thread.sleep;
 @Service
 public class SliderKzService {
 
+    public static final String HREF = "href";
+    public static final String A = "a";
+    public static final String LIVEAUDIO = "liveaudio";
+    public static final String STRIPE_ODD = "stripe-odd";
     private final String baseSearch = "http://slider.kz/#";
 
     public String searchSong(String sText, FirefoxDriver driver) {
         log.info("Search started: "+sText);
-        sText = sText.replace(" ","%20");
-        String sLink = baseSearch.concat(sText);
+        String dwnLink = "";
+        String searchLink = getSearchLink(sText);
         try{
-            driver.get(sLink);
-            FirefoxDriverUtils.tryWaitingForPageToLoad(driver, 10, By.className("stripe-odd"));
-            WebElement el = driver.findElement(By.id("liveaudio"));
-            WebElement dwn = el.findElement(By.tagName("a"));
-            String dwnLink = dwn.getAttribute("href");
-            log.info("found :"+dwnLink+"\n"+"in "+sLink);
-            sleep(500);
-            return dwnLink;
+            driver.get(searchLink);
+            FirefoxDriverUtils.tryWaitingForPageToLoad(driver, 10, By.className(STRIPE_ODD));
+            WebElement el = driver.findElement(By.id(LIVEAUDIO));
+            WebElement dwn = el.findElement(By.tagName(A));
+            dwnLink = dwn.getAttribute(HREF);
+
+            log.info("found :"+dwnLink+"\n"+"in "+searchLink);
         } catch (Exception e){
-            log.error("error finding Download Link in {}", sLink);
-            //e.printStackTrace();
-            try {
-                sleep(500);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-            return "";
+            log.error("error finding Download Link in {}", searchLink);
+        }
+        politeWaitTime();
+        return dwnLink;
+    }
+
+    private String getSearchLink(String sText) {
+        sText = sText.replace(" ","%20");
+        return baseSearch.concat(sText);
+    }
+
+    private void politeWaitTime() {
+        try {
+            sleep(500);
+        } catch (InterruptedException ex) {
+            log.error(ex.getMessage());
         }
     }
 }
